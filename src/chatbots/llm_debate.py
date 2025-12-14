e7078ddc60929e83dd9034b6f7d2bbe70e4a8d66
idmlnz <idmlnz@yahoo.com>
2025-12-13 16:50:13 -0800

    Rename chatbot.py to llm_debate.py

--- src/chatbots/llm_debate.py
+++ src/chatbots/llm_debate.py
@@ -1,23 +1,25 @@
-'''
-    This exercises is for using 2 LLM to debate 
-'''
-
+"""
+    This application simulates a dynamic debate between two large language models (LLMs) 
+		on the topic of student loan repayment, with each model representing opposing viewpoints. 
+		The goal is to provide an engaging, interactive experience that helps users explore 
+		both sides of this complex issue.
+"""
 
 import os
-import requests
 from dotenv import load_dotenv
 from openai import OpenAI
-from IPython.display import Markdown, display
 
-
+# Load OpenAI API key
 load_dotenv(override=True)
 openai_api_key = os.getenv('OPENAI_API_KEY')
 
 if openai_api_key:
     print(f"OpenAI API Key exists and begins {openai_api_key[:8]}")
 
+# Initialize OpenAI API
 openai = OpenAI()
 
+# Model and system messages
 gpt_model = "gpt-4.1-mini"
 claude_model = "claude-3-5-haiku-latest"
 
@@ -28,14 +30,15 @@ claude_system = "You are a chatbot in a debate  who does not favor Loan forgiven
 no matter what their socio-economic status.  For you a loan is a loan that needs to be, \
 repaid."
 
+# Initial messages
 gpt_messages = ["Hi there"]
 claude_messages = ["Hi"]
 
-
-'''
+"""
     Uses OpenAPI LLM
-'''
+"""
 def call_gpt():
+    """Send messages to GPT and return its response."""
     messages = [{"role": "system", "content": gpt_system}]
     for gpt, claude in zip(gpt_messages, claude_messages):
         messages.append({"role": "assistant", "content": gpt})
@@ -43,12 +46,12 @@ def call_gpt():
     response = openai.chat.completions.create(model=gpt_model, messages=messages)
     return response.choices[0].message.content
 
-
-'''
+"""
     If we have a Claude subscription we can use Claude LLM
         For now use OpenAPI LLM
-'''
+"""
 def call_claude():
+    """Send messages to Claude and return its response."""
     messages = [{"role": "system", "content": claude_system}]
     for gpt, claude_message in zip(gpt_messages, claude_messages):
         messages.append({"role": "user", "content": gpt})
@@ -57,17 +60,15 @@ def call_claude():
     response = openai.chat.completions.create(model=gpt_model, messages=messages)
     return response.choices[0].message.content
 
-
-'''
-    This is the main function that runs the chatbot.
-'''
 if __name__ == "__main__":
+		# Start debates
     gpt_messages = ["Hi there"]
     claude_messages = ["Hi"]
 
     print(f"### GPT:\n{gpt_messages[0]}\n")
     print(f"### Claude:\n{claude_messages[0]}\n")
 
+		# Simulate debate for 5 rounds
     for i in range(5):
         gpt_next = call_gpt()
         print(f"### GPT:\n{gpt_next}\n")
